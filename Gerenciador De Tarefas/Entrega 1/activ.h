41ab3ced8bf7cfd2bd324daf3c6ede0293e6ac1e
Nath <85901645+nathymn@users.noreply.github.com.>
2025-06-02 19:50:55 -0300

    struct ent 1

--- Gerenciador De Tarefas/Entrega 1/activ.h
+++ Gerenciador De Tarefas/Entrega 1/activ.h
@@ -3,6 +3,17 @@
 
 #include <stdlib.h>
 
+typedef struct
+{
+	int id;
+    char titulo[100];
+    char descricao[200];
+    char data[11]; //(formato "DD/MM/AAAA")
+    int prioridade; //(1 a 5) 
+    int status; // (0 = pendente, 1 = concluída)
+
+
+} Tarefa;
 
 
 #endif // ACTIV_H_INCLUDED
